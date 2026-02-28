ae05a43c1e0bf66f9eee512a84315018cced9e64
Jefferson Costa <jefersoncosta0205@outlook.com>
2026-02-27 23:05:56 -0300

    Atualizações de banco hospedado no supabase. Atualização na classe UsuarioSpeed.

--- back-end/controle-financeiro/controle-financeiro/src/main/java/com/controlefinanceiro/controle_financeiro/config/UsuarioSeed.java
+++ back-end/controle-financeiro/controle-financeiro/src/main/java/com/controlefinanceiro/controle_financeiro/config/UsuarioSeed.java
@@ -2,13 +2,25 @@ package com.controlefinanceiro.controle_financeiro.config;
 
 import com.controlefinanceiro.controle_financeiro.model.Usuario;
 import com.controlefinanceiro.controle_financeiro.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class UsuarioSeed implements CommandLineRunner {
 
     private final UsuarioRepository usuarioRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Value("${seed.admin.email}")
+    private String adminEmail;
+
+    @Value("${seed.admin.senha}")
+    private String adminSenha;
 
     public UsuarioSeed(UsuarioRepository usuarioRepository) {
         this.usuarioRepository = usuarioRepository;
@@ -21,8 +33,8 @@ public class UsuarioSeed implements CommandLineRunner {
 
             Usuario usuario = new Usuario();
             usuario.setNome("admin");
-            usuario.setEmail("admin@controle.com");
-            usuario.setSenha("123456");
+            usuario.setEmail(adminEmail);
+            usuario.setSenha(passwordEncoder.encode(adminSenha));
             usuario.setTipo_usuario("ADMIN");
             usuarioRepository.save(usuario);
 
